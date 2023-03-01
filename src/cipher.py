0c1bee9da322db1296f31343345c9df32c36b378
dxt99 <ericlouis19x@gmail.com>
2023-03-01 22:04:26 +0700

    fix: cipher assertion

--- src/cipher.py
+++ src/cipher.py
@@ -1,14 +1,15 @@
 class Cipher:
-    __size = 128//8
+    __key_size = 128//8
+    __block_size = 128//8
 
     def __init__(self, key: bytes):
-        assert(len(key)==self.__size)
+        assert(len(key)==self.__key_size)
         pass
 
     def encrypt(self, data: bytes):
-        assert(len(data)%self.__size == 0)
+        assert(len(data) == self.__block_size)
         return data[::-1]
 
     def decrypt(self, data: bytes):
-        assert(len(data)%self.__size == 0)
+        assert(len(data) == self.__block_size)
         return data[::-1]

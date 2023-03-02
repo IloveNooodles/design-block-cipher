7930fbfd1e620a2feef34bb1419d2b243a6469a0
IloveNooodles <mgarebaldhie80@gmail.com>
2023-03-02 18:00:43 +0700

    feat: fix assertion and adding expand functions

--- src/expand.py
+++ src/expand.py
@@ -1,2 +1,51 @@
-def expand(input: bytes, key: bytes): #32bit -> 64bit
-  pass
+from secrets import choice, randbelow, randbits
+
+
+# expand 32bit input to 64bit output
+def expand(input: bytes, key: bytes) -> bytes: 
+  assert(len(input) == 4)
+  assert(len(key) == 2)
+  
+  key_int = int.from_bytes(key, "big")
+  input_int = int.from_bytes(input, "big")
+  
+  # 2 bit dari input, 1 bit dari key, 1 bit dari (key << i) ^ (input << i)
+  # 0 -> 1 bit dari input
+  # 1 -> 1 bit dari key
+  # 2 -> 1 bit dari (key) ^ (input >> random(32))
+  
+  OPTIONS = [1, 2, 1, 0, 0, 0, 0, 0, 2, 0, 2, 0, 1, 1, 0, 2, 1, 1, 0, 0, 0, 1, 2, 2, 1, 2, 0, 0, 2, 2, 2, 0, 2, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 2, 1, 0, 1, 0, 0, 0, 1, 2, 0, 2, 1, 0, 2, 2, 0, 0, 0, 0, 1]
+  
+  input_shift_count = 0
+  key_shift_count = 0
+  
+  temp = []
+  for _ in range(64):
+      operation = choice(OPTIONS)
+      OPTIONS.remove(operation)
+      if operation == 0:
+          temp.append(str((input_int >> input_shift_count) & 1))
+          input_shift_count += 1
+      elif operation == 1:
+          temp.append(str((key_int >> key_shift_count) & 1))
+          key_shift_count += 1
+      else:
+          rand_num = randbelow(32)
+          to_add = (key_int) ^ (input_int << rand_num)
+          temp.append(str(to_add & 1))
+  
+  res = "".join(temp)
+  res = int(res, 2).to_bytes(8, "big")
+  
+  assert(len(res) == 8)
+  
+  return res
+
+if __name__ == "__main__":
+    input_number = randbits(32)
+    key = randbits(16)
+    
+    a, b = input_number.to_bytes(4, "big"), key.to_bytes(2, "big")
+    res = expand(a, b)
+    
+    print("RES: ", res)
--- src/pbox.py
+++ src/pbox.py
@@ -1,30 +1,33 @@
-import secrets
+from secrets import randbits
 
 PBOX = [49, 35, 58, 14, 28, 59, 51, 9, 18, 46, 55, 19, 20, 27, 26, 32, 17, 56, 45, 31, 41, 23, 40, 11, 57, 44, 61, 5, 12, 15, 4, 52, 6, 62, 21, 25, 8, 13, 0, 39, 30, 33, 43, 2, 47, 42, 29, 7, 22, 3, 48, 1, 63, 37, 10, 38, 60, 50, 16, 34, 36, 54, 53, 24] 
 
 LEN_PBOX = 64
 
-def permute(input: bytes):
+def permute(input: bytes) -> bytes:
+  assert(len(input) == 8) # 8 bytes
+  
   temp = []
   num = int.from_bytes(input, "big")
-  
-  assert(num.bit_length() == 64)
-  
+
   for i in range(LEN_PBOX):
     temp.append(str((num >> PBOX[i]) & 1))
   
   res = "".join(temp)
   res = int(res, 2).to_bytes(LEN_PBOX // 8, "big")
+  
+  assert(len(res) == 8)
+  
   return res
   
 
 if __name__ == "__main__":
-    num = secrets.randbits(64)
+    num = randbits(64)
     
     print("NUM: ", num)
     print("BIN: ", bin(num))
     
-    temp = permute(num.to_bytes(LEN_PBOX, "big"))
+    temp = permute(num.to_bytes(LEN_PBOX // 8, "big"))
     temp_int = int.from_bytes(temp, "big")
     
     print("NUM: ", temp_int)
